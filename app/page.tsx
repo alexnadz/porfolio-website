d166d963ece077bb5b5034369e01697a954b5e12
AlexNadz <nadeykoa@gmail.com>
2025-05-19 08:42:11 +0200

    feat: implement about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Hero } from '@/lib/components/home/hero/hero';
+import { About } from '@/lib/components/home/about/about';
 
 const HomePage = () => (
     <div className="scroll-smooth">
         <Hero />
+        <About />
     </div>
 );
 
